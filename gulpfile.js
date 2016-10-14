eafc2a3ba85d0241d3e852f2b237e58197acf179
木风 <niluanxy@163.com>
2016-10-14 17:55:43 +0800

    form表单组件样式修改

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ colors.setTheme({
     time: 'gray'
 });
 
-var DIR_MIXIN = "mixin/";
+var DIR_MIXIN = "mixin/",
+    DIST_MIXIN = DIR_MIXIN + "/dist";
 
 function log(str, style) {
     style = style || "info";
@@ -38,13 +39,13 @@ function log(str, style) {
  * mixin 文件合并脚本函数
  =================================================*/
 function task_concat_mixin() {
-    var MIXIN_DIST = DIR_MIXIN+"dist/",
+    var DIST_MIXIN = DIR_MIXIN+"dist/",
         defer_core = Q.defer(),
         defer_uikit = Q.defer(),
         defer_vars = Q.defer(),
         defer_all = Q.defer();
 
-    del(MIXIN_DIST).then(function() {
+    clear_mixin().then(function() {
         gulp.src([DIR_MIXIN+"core/config.scss",
             DIR_MIXIN+"core/function.scss",
             DIR_MIXIN+"core/zindex.scss",
@@ -52,18 +53,18 @@ function task_concat_mixin() {
             DIR_MIXIN+"core/text.scss",
             DIR_MIXIN+"core/base.scss"])
         .pipe(concat("mixin_core.scss"))
-        .pipe(gulp.dest(MIXIN_DIST))
+        .pipe(gulp.dest(DIST_MIXIN))
         .on("finish", function() { defer_core.resolve(); })
 
         gulp.src([DIR_MIXIN+"uikit/tools.scss",
             DIR_MIXIN+"uikit/component/**/*.scss"])
         .pipe(concat("mixin_uikit.scss"))
-        .pipe(gulp.dest(MIXIN_DIST))
+        .pipe(gulp.dest(DIST_MIXIN))
         .on("finish", function() { defer_uikit.resolve(); })
 
         gulp.src(DIR_MIXIN+"uikit/varible/**/*.scss")
         .pipe(concat("mixin_vars.scss"))
-        .pipe(gulp.dest(MIXIN_DIST))
+        .pipe(gulp.dest(DIST_MIXIN))
         .on("finish", function() { defer_vars.resolve(); })
 
         return Q.all([
@@ -72,14 +73,14 @@ function task_concat_mixin() {
             defer_vars.promise
         ])
     }).then(function() {
-        gulp.src([MIXIN_DIST+"mixin_core.scss",
-            MIXIN_DIST+"mixin_vars.scss",
-            MIXIN_DIST+"mixin_uikit.scss",
+        gulp.src([DIST_MIXIN+"mixin_core.scss",
+            DIST_MIXIN+"mixin_vars.scss",
+            DIST_MIXIN+"mixin_uikit.scss",
             DIR_MIXIN+"build.scss"])
         .pipe(concat("mixin.scss"))
-        .pipe(gulp.dest(MIXIN_DIST))
+        .pipe(gulp.dest(DIST_MIXIN))
         .pipe(sass())
-        .pipe(gulp.dest(MIXIN_DIST))
+        .pipe(gulp.dest(DIST_MIXIN))
         .on("finish", function() {
             log("all has finish");
             defer_all.resolve();
@@ -89,6 +90,14 @@ function task_concat_mixin() {
     return defer_all.promise;
 }
 
+function clear_mixin() {
+    return del(DIST_MIXIN);
+}
+
 gulp.task("build", function() {
     task_concat_mixin();
+})
+
+gulp.task("clean", function() {
+    clear_mixin();
 })
